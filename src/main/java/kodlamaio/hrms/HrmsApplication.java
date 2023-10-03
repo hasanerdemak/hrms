503da7c5041a2f9db36b00fa66eb2a6228bc9356
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-10-04 01:29:57 +0300

    swagger added

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -1,9 +1,24 @@
 package kodlamaio.hrms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "HRMS",
+                version = "1.0.0",
+                description = "HRMS project for Java & React Camp",
+                contact = @Contact(
+                        name = "Hasan Erdem Ak",
+                        email = "hasanerdemak@gmail.com"
+                )
+
+        )
+)
 public class HrmsApplication {
 
     public static void main(String[] args) {
