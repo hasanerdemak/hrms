a05b12d0ce6d9b20b12ba2ece842d1ed196f4c4a
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-10-29 11:14:06 +0300

    Req 3 completed

---
+++ src/main/java/kodlamaio/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,6 @@
+package kodlamaio.hrms.business.abstracts;
+
+import kodlamaio.hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService extends BaseEntityService<JobPosition, Integer> {
+}
